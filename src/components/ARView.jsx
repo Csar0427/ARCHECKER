982bced85fa5ecb8d0edace0fe4f7bf323bcab43
Csar0427 <jeromecesar27@gmail.com>
2024-09-26 14:05:37 +0800

    Updated AR functionality and fixed import issues

--- src/components/ARView.jsx
+++ src/components/ARView.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react';
-import 'aframe'; // Import A-Frame
 
 const ARView = ({ modelUrl }) => {
   useEffect(() => {
-    // Any additional setup can go here
+    // Any setup needed can be done here
   }, [modelUrl]);
 
   return (
-    <a-scene embedded>
-      <a-assets>
-        <a-asset-item id="model" src={modelUrl}></a-asset-item>
-      </a-assets>
-      <a-entity gltf-model="#model" scale="0.5 0.5 0.5" position="0 0 0"></a-entity>
-      <a-camera></a-camera>
+    <a-scene embedded arjs>
+      <a-marker preset="hiro">
+        <a-entity
+          gltf-model={modelUrl}
+          scale="0.5 0.5 0.5"
+          position="0 0 0"
+          rotation="0 0 0"
+        ></a-entity>
+      </a-marker>
+      <a-entity camera></a-entity>
     </a-scene>
   );
 };
