25ea2f203f96ca9406227e74d5a92bc809a40927
Csar0427 <jeromecesar27@gmail.com>
2024-09-26 12:57:00 +0800

    Your descriptive commit message here

--- src/components/ARView.js
+++ src/components/ARView.js
@@ -3,19 +3,15 @@ import 'aframe';
 
 const ARView = ({ modelUrl }) => {
   useEffect(() => {
-    // Additional setup or effects if needed
+    const modelEl = document.querySelector('#ar-model');
+    if (modelEl) {
+      modelEl.setAttribute('gltf-model', modelUrl);
+    }
   }, [modelUrl]);
 
   return (
     <a-scene embedded>
-      <a-marker preset="hiro">
-        <a-entity
-          gltf-model={modelUrl}
-          scale="0.5 0.5 0.5"
-          position="0 0 0"
-          rotation="0 0 0"
-        ></a-entity>
-      </a-marker>
+      <a-entity id="ar-model" scale="0.5 0.5 0.5" position="0 0 0"></a-entity>
       <a-entity camera></a-entity>
     </a-scene>
   );
